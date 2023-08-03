44af121a1c07cadcfe532538df1ba6b95edebc40
MahmoudDawood <mm.dawood16@yahoo.com>
2023-08-03 18:10:39 +0300

    Create adoption mddal using portal

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import Modal from "./Modal";
 import fetchPet from "./fetchPet";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
   const { id } = useParams();
 
   const result = useQuery(["details", id], fetchPet);
@@ -28,11 +32,28 @@ const Details = () => {
       <div>
         <h1>{pet.name}</h1>
         <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h2>Do you want to adopt {pet.name}?</h2>
+              <div className="buttons">
+                <button>Yes</button>
+                <button onClick={() => setShowModal(false)}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
         <p>{pet.description}</p>
       </div>
     </div>
   );
 };
 
-export default Details;
+export default function DetailsErrorBoundary(props) {
+  return (
+    <ErrorBoundary>
+      <Details {...props} />
+    </ErrorBoundary>
+  );
+}
